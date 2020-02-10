28b41f9204b4cf9688a566916884efa7ad355b84
Chase Wilson <contact@chasewilson.dev>
2020-02-09 19:01:38 -0600

    Added switch to dev branch in Fuzz

--- fuzzing/fuzz.sh
+++ fuzzing/fuzz.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 sudo apt install build-essential binutils-dev libunwind-dev libblocksruntime-dev
 cargo install honggfuzz
 HFUZZ_RUN_ARGS="--threads=8 --linux_perf_instr --linux_perf_branch --timeout=2 --tmout_sigvtalrm --verifier"
