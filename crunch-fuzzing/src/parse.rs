fdff1ca39197e26caf425193dfb4f44514952f84
Chase Wilson <contact@chasewilson.dev>
2020-02-29 22:01:38 -0600

    Working on Fuzzing

--- crunch-fuzzing/src/parse.rs
+++ crunch-fuzzing/src/parse.rs
@@ -4,7 +4,7 @@ extern crate honggfuzz;
 fn main() {
     loop {
         fuzz!(|bytes: &[u8]| {
-            if Ok(input_str) = std::str::from_utf8(bytes) {
+            if let Ok(input_str) = std::str::from_utf8(bytes) {
                 let _ = crunch::Parser::new(None, &input_str).parse();
             }
         });
