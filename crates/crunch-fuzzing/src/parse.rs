220510967ca24ef51a45379b00ce80d9b89479a1
Chase Wilson <contact@chasewilson.dev>
2020-03-27 09:37:22 -0500

    Working on fuzzing

---
+++ crates/crunch-fuzzing/src/parse.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate honggfuzz;
+
+use crunch_parser::files::FileId;
+use parking_lot::RwLock;
+use std::sync::Arc;
+
+fn main() {
+    let interner = Arc::new(RwLock::new(
+        crunch_parser::string_interner::StringInterner::new(),
+    ));
+
+    loop {
+        fuzz!(|bytes: &[u8]| {
+            if let Ok(input_str) = std::str::from_utf8(bytes) {
+                let _ =
+                    crunch_parser::Parser::new(&input_str, FileId::new(0), Arc::clone(&interner))
+                        .parse();
+            }
+        });
+    }
+}
