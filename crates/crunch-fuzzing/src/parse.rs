e90832d620f1d7fc4702881d45d62ebb5b455255
Chase Wilson <contact@chasewilson.dev>
2020-03-27 11:16:54 -0500

    Fixed parser timeouts

--- crates/crunch-fuzzing/src/parse.rs
+++ crates/crunch-fuzzing/src/parse.rs
@@ -1,6 +1,5 @@
 use crunch_parser::{files::FileId, Interner};
 use honggfuzz::fuzz;
-use std::sync::Arc;
 
 fn main() {
     let interner = Interner::new();
@@ -8,9 +7,8 @@ fn main() {
     loop {
         fuzz!(|bytes: &[u8]| {
             if let Ok(input_str) = std::str::from_utf8(bytes) {
-                let _ =
-                    crunch_parser::Parser::new(&input_str, FileId::new(0), Arc::clone(&interner))
-                        .parse();
+                let _ = crunch_parser::Parser::new(&input_str, FileId::new(0), interner.clone())
+                    .parse();
             }
         });
     }
